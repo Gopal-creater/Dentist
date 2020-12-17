3a24e7073d84d60da52cd0ea5fb94a68240c8878
bharat.karnani6 <bharat.karnani6@gmail.com>
2020-12-17 12:34:15 +0530

    about us and services page created

--- src/Component/Carousel_show2/CarouselShow2.js
+++ src/Component/Carousel_show2/CarouselShow2.js
@@ -3,55 +3,85 @@ import './CarouselShow2.scss';
 import Slider from "react-slick";
 import sliderpic3 from '../../assets/Group 494.png';
 import sliderpic4 from '../../assets/Group 495.png';
-
 export default function CarouselShow2() {
 
     const NextArrow = ({ onClick }) => {
         return (
-            <div className="arrows next_button_2" onClick={onClick}>
-                {/* <button style={{outline:'none'}} type="submit" className="previous2 round2">
+            <div className="arrow_test next_button_test" onClick={onClick}>
+                <button style={{outline:'none'}} type="submit" className="previous2_test round2_test">
                     &#8250;
-              </button> */}
-                <h1>Next</h1>
+              </button>
+                {/* <h1>Next</h1> */}
             </div>
         );
     };
     const PrevArrow = ({ onClick }) => {
         return (
-            <div className="arrows prev_button" onClick={onClick} style={{marginTop:'-300px', marginLeft:'10px'}}>
-                <button style={{ outline: 'none' }} type="submit" className="previous1 round1">
+            <div className="arrow_test prev_button_test" onClick={onClick} style={{ marginTop: '-300px', marginLeft: '10px' }}>
+                <button style={{ outline: 'none' }} type="submit" className="previous1_test round1_test">
                     &#8249;
               </button>
+                {/* <h1>Prev</h1> */}
             </div>
         );
     };
-    const images = [sliderpic3, sliderpic4];
+    const images = [sliderpic3, sliderpic3, sliderpic4];
     const [imageIndex, setImageIndex] = useState(0);
     const settings = {
         infinite: true,
         lazyLoad: true,
-        speed: 2000,
+        speed: 300,
         slidesToShow: 2,
+        centerMode: true,
+        centerPadding: 47,
         nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
         beforeChange: (current, next) => setImageIndex(next),
+        responsive: [
+            {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              centerMode: true,
+              centerPadding: 0,
+            }}
+        ]
     };
     return (
-        <div>
-            <div className="carouselshow2">
-                <div className="pacth_carouselshow2"></div>
-                <Slider {...settings}>
-                    {images.map((img, idx) => (
-                        <div className={idx === imageIndex ? "slidess activeSlidess" : "slidess"}>
-                            {/* <div className="images_carouselshow2"> */}
-                                <img src={img} />
-                            {/* </div> */}
-                        </div>
-                    ))}
-                </Slider>
+        <div className="row">
+            <div className="col-md-6 col-12">
+                <div className="patch_test">
+                    <div className="App_test">
+                        <Slider {...settings}>
+                            {images.map((img, idx) => (
+                                <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+                                    <img src={img} alt={img} />
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
+                    
+                </div>
+                <div class="three-dots_test">
+                        <span class="first"></span>
+                        <span class="first"></span>
+                        <span class="first"></span>
+                        <span class="first"></span>
+                        <span class="first"></span>
+                        <span class="first"></span>
+                        <span class="second"></span>
+                        <span class="second"></span>
 
-
+                    </div>
+            </div>
+            <div className="col-md-6 col-12" id="text_test">
+                <h1>How is<br /> it done?</h1>
+                <p>Dental crowns are fabricated in the dental laboratory using the impression your dentist has made of your tooth after having prepared it. Because dental crowns encase the visible portion of a tooth, any porcelain crown (all-ceramic or porcelain-fused-to-metal ones) can be used to enhance the cosmetic appearance of a tooth.</p>
             </div>
 
         </div>
+
     );
 }
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import NavBar from '../../Component/NavBar/NavBar';
+import Header from '../servicesPage/header/header';
+import Expert from '../../assets/expert1.png';
+import './About.scss'
+import CarouselShow3 from '../../Component/Carousel_show3/CarouselShow3';
 
 export default function About() {
     return (
         <div>
-            This is About
+            <Header />
+            <div className="navbar_about">
+                <NavBar />
+            </div>
+            <div className="about_heading">
+                <h1 style={{ fontFamily: 'Si-ui-bold', color: '#3AA9A9' }}>About</h1>
+            </div>
+            <div className="about_image">
+                <div className="about_patch"></div>
+                <div className="about_doctor_image">
+                    <img src={Expert} />
+                </div>
+            </div>
+            <div className="about_longtext">
+                <h1>The Story of<br /> Shwetha Dentistry</h1>
+                <p>Shwetha Dentistry in Kalaburagi city is proving itself to be a trendsetter in bringing the latest world-class treatment facilities at an affordable cost to the doorsteps of needy patients in Karnataka. Be it establishing a super speciality dental hospital or introducing altogether a new concept of one-visit dentistry, Shwetha Dentistry has many firsts not only in Hyderabad-Karnataka region but also in the whole of Karnataka.</p>
+                <br />
+                <h1>The Man, The Myth, The Legend.</h1>
+                <p>The man behind the awe-inspiring success story of Shwetha Dentistry is Dr Prashant A Patil whose unmatched pioneering efforts in introducing modern treatment facilities in dentistry has set a new standard in providing painless and cosmetic treatment procedures and brightening the smiles on the faces of the patients. A post-graduate in Orthodontics from the prestigious Raja Muthaiah Institute of Dental Sciences, Annamalai University at Chidambaram in Tamil Nadu, Dr Prashant Patil obtained a specialisation in Lingual Orthodontics (Invisible Braces) course offered by Tel Aviv University through European Lingual Orthodontic Society in London. Incidentally, Dr Patil was the only Asian in 2010 to obtain the specialisation in Lingual Orthodontics from Tel Aviv University. The never-say-die attitude of Dr Patil and his unswerving resolve to achieve only the best in the trade has moulded him as one of the finest professionals and a trailblazer.</p>
+                <br />
+                <h1>A Team of Titans</h1>
+                <p>Besides the self-confidence and sheer hard work of Dr Patil, another force which stands tall like a rock behind him in his phenomenal success in the profession is his wife Dr Rashmi Patil who herself is a specialist endodontist. Apart from Dr Patil and his wife Dr Reshmi Patil, seven other specialists work as consultants in the hospital.</p>
+                <h1>Laurels upon laurels.</h1>
+                <p>Shwetha Dentistry is one of the very few centres in the state to get ISO 2015-18 from the prestigious TUV Nord Sud Certification from Germany. Now the hospital has applied for the accreditation from the NABH, which has been made mandatory for all the hospitals. The hospital has received many awards and recognition from different certification organisations and prominent among them was the FAMDENT excellence in Dentistry award which declared the Shwetha Dentistry has the clinic of the year Multiple Chairs 2013. Dr Patil says that the main motto of the Shwetha Dentistry is to provide world-class quality and precision service to the patients.</p>
+            </div>
+            <div className="about_image_2">
+                <div className="about_patch_2"></div>
+                <div className="about_doctor_image_2">
+                    <img src={Expert} />
+                </div>
+            </div>
+            <div className="about_longtext_2">
+                <h1>Inspired by Technology, Powered by Innovation.</h1>
+                <p>Dr Patil and his hospital have many firsts to their credit. The state of art Digital Dentistry Complete Ceramic Reconstruction (CEREC) CADCAM System, which has revolutionised the cosmic dental treatment procedures was first established in entire South India in Kalaburagi. Shwetha Dentistry was the 13th hospital in the entire country to have this facility.<br /><br /> This innovative facility has enabled the hospital to introduce an altogether new concept of “One Visit Dentistry” using the CEREC system. The teeth which require the crown after the Root Canal Treatment and filling is digitally scanned for a digital impression of the teeth. The impression is transferred to the robotic milling machine which designs the crown from the pre-fixed Lithium Disilicate material imported from Germany to the size required within 15 minutes. After following a few technical procedures, like hardening the designed crown to the required level, the crown is placed in the mouth of the patient within three hours on the same day. <br /><br />Shwetha Dentistry is the first to include the most modern Microscope-assisted Root Canal Treatment facility which helps in bringing down the loss of tooth material to zero during the root canal treatment and also accurately treat the affected root.<br /><br /> The Hospital offers nine specialised treatments for dental ailments including Orthodontics, Endodontic, Oral Surgery, Periodontics, Dental Implants, Implantology, Prosthodontics, Laser Gum Treatment, Oral Medicine, and Oral Radiology.<br /><br /> Besides all these specialisations offered in the hospital, one of the rare specialisations offered is Lingual Orthodontics. Shwetha Dentistry is the exclusive hospital to offer the lingual orthodontics specialisation in the entire North Karnataka. Dr Patil has a rich experience of nearly a decade in offering specialised lingual orthodontics (invisible) treatment.<br /><br /> The patients suffering from irregular teeth alignment are offered permanent treatment solutions through Invisalign treatment using the latest kind of invisible braces. Shwetha Dentistry is an exclusive Invisalign provider for the entire North Karnataka.<br /><br /> The hospital also provides laser dental surgeries which are painless and bloodless surgeries are also provided for different gum diseases and the healing process was faster in the patients who undergo laser-based dental surgeries. The hospital also offers instant teeth whitening in one single sitting using the imported Zoom Teeth whitening technology. Advanced 3D radiology facility is also available in the hospital.</p>
+                <br />
+                <h1>Laurels upon laurels.</h1>
+                <p>Shwetha Dentistry is one of the very few centres in the state to get ISO 2015-18 from the prestigious TUV Nord Sud Certification from Germany. Now the hospital has applied for the accreditation from the NABH, which has been made mandatory for all the hospitals. The hospital has received many awards and recognition from different certification organisations and prominent among them was the FAMDENT excellence in Dentistry award which declared the Shwetha Dentistry has the clinic of the year Multiple Chairs 2013. <br /><br />Dr Patil says that the main motto of the Shwetha Dentistry is to provide world-class quality and precision service to the patients.</p>
+            </div>
+            <div className="about_heading_2 text-center">
+                <h1 style={{ fontFamily: 'Si-ui-bold' }}>Meet our Experts</h1>
+            </div>
+            <CarouselShow3 />
         </div>
     )
 }

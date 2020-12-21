37cac3a897b645ea3c879edd646f1eda5b1c3d15
GayathriRaja <69466464+GayathriRaja@users.noreply.github.com>
2020-12-21 18:34:20 +0530

    changes_progressBar_ImageProps_in_About_services_page_fix_resizing_issue_in_heading_tooth_component

--- src/Component/Carousel_show3/CarouselShow3.js
+++ src/Component/Carousel_show3/CarouselShow3.js
@@ -1,64 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import './CarouselShow3.scss';
+import React, { useEffect, useState } from "react";
+import "./CarouselShow3.scss";
 import Slider from "react-slick";
-import astronaut from "../../assets/Group 693.png";
-import celebrating from "../../assets/Group 691.png";
-import education from "../../assets/Group 690.png";
+// import astronaut from "../../assets/Group 693.png";
+// import celebrating from "../../assets/Group 691.png";
+// import education from "../../assets/Group 690.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+export default function CarouselShow3(props) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-export default function CarouselShow3() {
-
-    useEffect(() => {
-        AOS.init();
-      }, []);
-
-    const NextArrow = ({ onClick }) => {
-        return (
-            <div className="arrows next_button_services" onClick={onClick}>
-                <button style={{outline:'none'}} type="submit" className="previous2 round2">
-                    &#8250;
-              </button>
-                {/* <h1>Next</h1> */}
-            </div>
-        );
-    };
-    const PrevArrow = ({ onClick }) => {
-        return (
-            <div className="arrows prev_button_services" onClick={onClick} style={{ marginTop: '-300px', marginLeft: '10px' }}>
-                <button style={{ outline: 'none' }} type="submit" className="previous1 round1">
-                    &#8249;
-              </button>
-              {/* <h1>Prev</h1> */}
-            </div>
-        );
-    };
-    const images = [astronaut, celebrating, education, celebrating];
-    const [imageIndex, setImageIndex] = useState(0);
-    const settings = {
-        infinite: true,
-        lazyLoad: true,
-        speed: 300,
-        slidesToShow: 3,
-        centerMode: true,
-        centerPadding: 0,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        beforeChange: (current, next) => setImageIndex(next),
-    };
+  const NextArrow = ({ onClick }) => {
     return (
-        <div>
-            <div className="carasouel_services" data-aos="zoom-in" data-aos-duration="3000">
-                <Slider {...settings}>
-                    {images.map((img, idx) => (
-                        <div className={idx === imageIndex ? "slide_services activeSlide_services" : "slide_services"}>
-                            <img src={img} alt={img} />
-                        </div>
-                    ))}
-                </Slider>
-            </div>
-        </div>
-
+      <div className="arrows next_button_services" onClick={onClick}>
+        <button
+          style={{ outline: "none" }}
+          type="submit"
+          className="previous2 round2"
+        >
+          &#8250;
+        </button>
+        {/* <h1>Next</h1> */}
+      </div>
     );
+  };
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div
+        className="arrows prev_button_services"
+        onClick={onClick}
+        style={{ marginTop: "-300px", marginLeft: "10px" }}
+      >
+        <button
+          style={{ outline: "none" }}
+          type="submit"
+          className="previous1 round1"
+        >
+          &#8249;
+        </button>
+        {/* <h1>Prev</h1> */}
+      </div>
+    );
+  };
+  // const images = [astronaut, celebrating, education, celebrating];
+  const images = [props.slider1, props.slider2, props.slider3, props.slider1];
+
+  const [imageIndex, setImageIndex] = useState(0);
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+  return (
+    <div>
+      <div
+        className="carasouel_services"
+        data-aos="zoom-in"
+        data-aos-duration="3000"
+      >
+        <Slider {...settings}>
+          {images.map((img, idx) => (
+            <div
+              className={
+                idx === imageIndex
+                  ? "slide_services activeSlide_services"
+                  : "slide_services"
+              }
+            >
+              <img src={img} alt={img} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
 }
--- src/Component/Heading_mouseover_mobile/HeadingMouseover_mobile.js
+++ src/Component/Heading_mouseover_mobile/HeadingMouseover_mobile.js
@@ -1,85 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import './HeadingMouseover_mobile.scss';
-import heading1 from '../../assets/mobile_text.png';
-import heading2 from '../../assets/mobile_text_2.png';
-import Ring from '../../assets/Group 679.png';
-import Parallax from 'react-rellax';
+import React, { useState, useEffect } from "react";
+import "./HeadingMouseover_mobile.scss";
+import heading1 from "../../assets/mobile_text.png";
+import heading2 from "../../assets/mobile_text_2.png";
+import Ring from "../../assets/Group 679.png";
+import Parallax from "react-rellax";
 
 export default function HeadingMouseoverMobile() {
-    useEffect(() => {
-        heading();
-    })
+  useEffect(() => {
+    heading();
+  });
 
-
-    function heading() {
-        // tslint:disable-next-line:no-unused-expression
-        // tslint:disable-next-line:prefer-const
-        const svgElement1 = document.getElementById('animation_heading_mobile');
-        const maskedElement1 = document.querySelector('#mask-circle_heading_mobile');
-        const circleFeedback1 = document.querySelector('#circle-shadow_heading_mobile');
-        const svgPoint = svgElement1.createSVGPoint();
-        // tslint:disable-next-line:typedef
-        function cursorPoint(e, svg) {
-            svgPoint.x = e.clientX;
-            svgPoint.y = e.clientY;
-            return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
-        }
-        // tslint:disable-next-line:typedef
-        function update(svgCoords) {
-            maskedElement1.setAttribute('cx', svgCoords.x);
-            maskedElement1.setAttribute('cy', svgCoords.y);
-            circleFeedback1.setAttribute('cx', svgCoords.x);
-            circleFeedback1.setAttribute('cy', svgCoords.y);
-        }
-        // tslint:disable-next-line:typedef
-        // tslint:disable-next-line:only-arrow-functions
-        window.addEventListener('mousemove', (e) => {
-            const scrol = window.pageYOffset + e.clientY;
-            if (scrol > 130 && scrol < 200) {
-                update(cursorPoint(e, svgElement1));
-                document.getElementById('circle-shadow_heading_mobile').style.display = 'block';
-                document.getElementById('mask-circle_heading_mobile').style.display = 'block';
-            } else {
-                document.getElementById('circle-shadow_heading_mobile').style.display = 'none';
-                document.getElementById('mask-circle_heading_mobile').style.display = 'none';
-            }
-
-
-        }, false);
-        document.addEventListener('touchmove', (e) => {
-            e.preventDefault();
-            const touch = e.targetTouches[0];
-            if (touch) {
-                update(cursorPoint(touch, svgElement1));
-            }
-        }, false);
-    }
-
-    return (
-        <div>
-            <Parallax speed={-4}>
-                <svg className="heading_image_mobile" id="animation_heading_mobile">
-                    <rect width="100%" height="100%" fill="yellow"
-                        fillOpacity="0"
-                    />
-                    <image x='35' xlinkHref={heading1} width="300" height="300" />
-                </svg>
-                <svg className="heading_image_mobile" id="animation_heading_mobile">
-                    <defs>
-                        <clipPath id="mask1_mobile">
-                            <circle id="mask-circle_heading_mobile" cx="50%" cy="50%" r="5%" style={{ fill: '#ffffff' }} />
-                        </clipPath>
-                        <filter id="this_image_heading_mobile" x="0%" y="0%" width="100%" height="100%">
-                            <feImage cx="50%" cy="50%" r="6%" xlinkHref={Ring} />
-                        </filter>
-                    </defs>
-                    <g clipPath="url(#mask1_mobile)">
-                        <rect width="100%" height="100%" fill="#175C64" />
-                        <image x='35' xlinkHref={heading2} width="300" height="300" />
-                    </g>
-                    <circle id="circle-shadow_heading_mobile" cx="50%" cy="50%" r="6%" filter="url(#this_image_heading_mobile)" />
-                </svg>
-            </Parallax>
-        </div>
+  function heading() {
+    // tslint:disable-next-line:no-unused-expression
+    // tslint:disable-next-line:prefer-const
+    const svgElement1 = document.getElementById("animation_heading_mobile");
+    const maskedElement1 = document.querySelector(
+      "#mask-circle_heading_mobile"
     );
+    const circleFeedback1 = document.querySelector(
+      "#circle-shadow_heading_mobile"
+    );
+    const svgPoint = svgElement1.createSVGPoint();
+    // tslint:disable-next-line:typedef
+    function cursorPoint(e, svg) {
+      svgPoint.x = e.clientX;
+      svgPoint.y = e.clientY;
+      return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
+    }
+    // tslint:disable-next-line:typedef
+    function update(svgCoords) {
+      maskedElement1.setAttribute("cx", svgCoords.x);
+      maskedElement1.setAttribute("cy", svgCoords.y);
+      circleFeedback1.setAttribute("cx", svgCoords.x);
+      circleFeedback1.setAttribute("cy", svgCoords.y);
+    }
+    // tslint:disable-next-line:typedef
+    // tslint:disable-next-line:only-arrow-functions
+    window.addEventListener(
+      "mousemove",
+      (e) => {
+        const scrol = window.pageYOffset + e.clientY;
+        if (scrol > 130 && scrol < 200) {
+          update(cursorPoint(e, svgElement1));
+          document.getElementById(
+            "circle-shadow_heading_mobile"
+          ).style.display = "block";
+          document.getElementById("mask-circle_heading_mobile").style.display =
+            "block";
+        } else {
+          document.getElementById(
+            "circle-shadow_heading_mobile"
+          ).style.display = "none";
+          document.getElementById("mask-circle_heading_mobile").style.display =
+            "none";
+        }
+      },
+      false
+    );
+    document.addEventListener(
+      "touchmove",
+      (e) => {
+        e.preventDefault();
+        const touch = e.targetTouches[0];
+        if (touch) {
+          update(cursorPoint(touch, svgElement1));
+        }
+      },
+      false
+    );
+  }
+
+  return (
+    <div>
+      <Parallax speed={-4}>
+        <svg className="heading_image_mobile" id="animation_heading_mobile">
+          <rect width="100%" height="100%" fill="yellow" fillOpacity="0" />
+          {/* <image x="35" xlinkHref={heading1} width="300" height="300" /> */}
+          <image x="9%" xlinkHref={heading1} width="80%" height="80%" />
+        </svg>
+        <svg className="heading_image_mobile" id="animation_heading_mobile">
+          <defs>
+            <clipPath id="mask1_mobile">
+              <circle
+                id="mask-circle_heading_mobile"
+                cx="50%"
+                cy="50%"
+                r="5%"
+                style={{ fill: "#ffffff" }}
+              />
+            </clipPath>
+            <filter
+              id="this_image_heading_mobile"
+              x="0%"
+              y="0%"
+              width="100%"
+              height="100%"
+            >
+              <feImage cx="50%" cy="50%" r="6%" xlinkHref={Ring} />
+            </filter>
+          </defs>
+          <g clipPath="url(#mask1_mobile)">
+            <rect width="100%" height="100%" fill="#175C64" />
+            {/* <image x="35" xlinkHref={heading2} width="300" height="300" /> */}
+            <image x="9%" xlinkHref={heading2} width="80%" height="80%" />
+          </g>
+          <circle
+            id="circle-shadow_heading_mobile"
+            cx="50%"
+            cy="50%"
+            r="6%"
+            filter="url(#this_image_heading_mobile)"
+          />
+        </svg>
+      </Parallax>
+    </div>
+  );
 }
